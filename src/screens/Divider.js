f1e9ab3b868dd62b06325753780d40098e9acbb8
ManavJain2210 <manavjain709@gmail.com>
2020-05-04 19:07:12 +0530

    Bugs Fixed

--- src/screens/Divider.js
+++ src/screens/Divider.js
@@ -1,21 +1,32 @@
 import firestore from '@react-native-firebase/firestore'
+import auth from '@react-native-firebase/auth'
 import React from 'react'
 import {ActivityIndicator , View} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
 
 export default class Divider extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            email: this.props.navigation.getParam('email', 'random@gmail.com'), 
+            email: auth().currentUser.email, 
+            type: '',
         }
     }
-    componentDidMount(){
-        firestore().collection('Users').doc(this.state.email).onSnapshot(querySnapshot =>{
-            if(querySnapshot.data().type == 'Buyer'){
-                this.props.navigation.navigate('HomeRoute')
+    componentDidMount = async() =>{
+        firestore().collection('Users').doc(this.state.email).onSnapshot(async(querySnapshot) =>{
+            console.log(querySnapshot)
+            if(querySnapshot.data()){
+                if(querySnapshot.data().type == 'Buyer')
+                    this.props.navigation.navigate('HomeRoute')
+                else
+                    this.props.navigation.navigate('SellerHomeRoute')
             }
             else{
-                this.props.navigation.navigate('SellerHomeRoute')
+                const type = await  AsyncStorage.getItem('type')
+                this.props.navigation.navigate('CompanyDetailsRoute', {
+                    type: type,
+                    email: this.state.email,
+                })
             }
         })
     }
